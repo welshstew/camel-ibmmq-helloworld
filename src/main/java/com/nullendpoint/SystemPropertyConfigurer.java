44f4a400852fad99eb3f8af7d83bc57eeaaabf71
Stuart Winchester <stuart.winchester@gmail.com>
2017-03-03 09:10:35 +0000

    added JAVA_SECURITY_PATH as system property

--- src/main/java/com/nullendpoint/SystemPropertyConfigurer.java
+++ src/main/java/com/nullendpoint/SystemPropertyConfigurer.java
@@ -5,8 +5,10 @@ package com.nullendpoint;
  */
 public class SystemPropertyConfigurer {
 
-    public SystemPropertyConfigurer(String trustStoreLocation, String trustStorePassword){
+    public SystemPropertyConfigurer(String trustStoreLocation, String trustStorePassword, String javaSecurityPath){
         System.setProperty("javax.net.ssl.trustStore", trustStoreLocation);
         System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword);
+        System.setProperty("java.security.properties", javaSecurityPath);
+        System.setProperty("security.overridePropertiesFile", "true");
     }
 }
